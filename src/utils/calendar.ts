74fe8136b5f98c942a2c444e3117c492b0f0ae06
KohnoA <san.kohno@yandex.ru>
2024-02-11 19:17:42 +0300

    refactor: calendar utils function

--- src/utils/calendar.ts
+++ src/utils/calendar.ts
@@ -1,72 +1,71 @@
 import { WEEK_DAYS_NAME } from '@/constants';
 import { DayType } from '@/types';
 
-export function generateCalendarData(currentDate: Date): DayType[] {
-  const generateDaysData = (
-    year: number,
-    month: number,
-    start: number,
-    end: number,
-    options?: { isCurrentMonth: boolean },
-  ) =>
-    new Array(end - start + 1).fill(start).map((_, index) => {
-      const dayDate = new Date(year, month, start + index);
+function generateDaysData(
+  year: number,
+  month: number,
+  start: number,
+  end: number,
+  options?: { isCurrentMonth: boolean },
+) {
+  const NAMES_OF_WEEKENDS = ['Su', 'Sa'];
 
-      const day = dayDate.getDate();
-      const timestamp = dayDate.getTime();
-      const weekDay = WEEK_DAYS_NAME[dayDate.getDay()];
-      const isWeekend = ['Su', 'Sa'].includes(weekDay);
+  return new Array(end - start + 1).fill(start).map((_, index) => {
+    const dayDate = new Date(year, month, start + index);
 
-      return {
-        day,
-        weekDay,
-        timestamp,
-        isCurrentMonth: options?.isCurrentMonth ?? false,
-        todos: null,
-        isWeekend,
-        isHoliday: false,
-      };
-    });
+    const day = dayDate.getDate();
+    const timestamp = dayDate.getTime();
+    const weekDay = WEEK_DAYS_NAME[dayDate.getDay()];
+    const isWeekend = NAMES_OF_WEEKENDS.includes(weekDay);
+    const isCurrentMonth = options?.isCurrentMonth ?? false;
 
-  const countDaysInMonth = new Date(
-    currentDate.getFullYear(),
-    currentDate.getMonth() + 1,
-    0,
-  ).getDate();
-  const firstWeekDayOfMonth = new Date(
-    currentDate.getFullYear(),
-    currentDate.getMonth(),
-    1,
-  ).getDay();
+    return {
+      day,
+      weekDay,
+      timestamp,
+      isCurrentMonth,
+      isWeekend,
+      todos: null,
+      isHoliday: false,
+    };
+  });
+}
 
-  const countDaysInPrevMonth = new Date(
-    currentDate.getFullYear(),
-    currentDate.getMonth(),
-    0,
-  ).getDate();
+export function generateCalendarData(date: Date): DayType[] {
+  const NUMBER_OF_CALENDAR_ITEMS = 42;
+  const currentDate = new Date(date.getTime());
+
+  currentDate.setMonth(currentDate.getMonth() + 1);
+  currentDate.setDate(0);
+  const countDaysInCurrentMonth = currentDate.getDate();
+
+  currentDate.setDate(0);
+  const countDaysInPrevMonth = currentDate.getDate();
+
+  currentDate.setDate(currentDate.getDate() + 1);
+  const startOfVisiblePrevMonth = countDaysInPrevMonth - (currentDate.getDay() - 1);
 
   const endOfPrevMonthData = generateDaysData(
     currentDate.getFullYear(),
     currentDate.getMonth() - 1,
-    countDaysInPrevMonth - (firstWeekDayOfMonth - 1),
+    startOfVisiblePrevMonth,
     countDaysInPrevMonth,
   );
+
   const currentMonthData = generateDaysData(
     currentDate.getFullYear(),
     currentDate.getMonth(),
     1,
-    countDaysInMonth,
+    countDaysInCurrentMonth,
     { isCurrentMonth: true },
   );
-
-  const test = [...endOfPrevMonthData, ...currentMonthData];
 
   const startOfNextMonthData = generateDaysData(
     currentDate.getFullYear(),
     currentDate.getMonth() + 1,
     1,
-    42 - test.length,
+    NUMBER_OF_CALENDAR_ITEMS - (endOfPrevMonthData.length + currentMonthData.length),
   );
 
-  return [...test, ...startOfNextMonthData];
+  return [...endOfPrevMonthData, ...currentMonthData, ...startOfNextMonthData];
 }
