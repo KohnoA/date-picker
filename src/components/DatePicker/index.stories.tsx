3dc12906d5d260f736706014369b8cfaaeecc6bf
KohnoA <san.kohno@yandex.ru>
2024-02-16 05:38:21 +0300

    feat: update stories for DatePicker

--- src/components/DatePicker/index.stories.tsx
+++ src/components/DatePicker/index.stories.tsx
@@ -1,14 +1,47 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
+import { CalendarView, CalendarWeekStart } from '@/constants';
+
 import { DatePicker } from './index';
 
 const meta: Meta<typeof DatePicker> = {
   component: DatePicker,
+  argTypes: {
+    label: {
+      control: { type: 'text' },
+    },
+    view: {
+      options: CalendarView,
+      control: 'select',
+    },
+    initialDate: {
+      control: 'date',
+    },
+    weekStart: {
+      options: CalendarWeekStart,
+      control: 'select',
+    },
+    min: {
+      control: 'date',
+    },
+    max: {
+      control: 'date',
+    },
+    showHolidays: {
+      control: 'boolean',
+    },
+    showWeekends: {
+      control: 'boolean',
+    },
+    colorOptions: {
+      control: 'object',
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof DatePicker>;
 
-export const FirstStory: Story = {
+export const Default: Story = {
   render: () => <DatePicker />,
 };
