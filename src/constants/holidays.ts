ad073e024952fd29f0fbe3d70b3b75393f2cef76
KohnoA <san.kohno@yandex.ru>
2024-02-19 01:08:33 +0300

    feat: update holidays data

--- src/constants/holidays.ts
+++ src/constants/holidays.ts
@@ -2,4 +2,9 @@ export const HOLIDAYS = [
   new Date(2024, 1, 14).getTime(),
   new Date(2024, 1, 23).getTime(),
   new Date(2024, 2, 8).getTime(),
+  new Date(2024, 3, 15).getTime(),
+  new Date(2024, 5, 9).getTime(),
+  new Date(2024, 7, 3).getTime(),
+  new Date(2024, 1, 1).getTime(),
+  new Date(2024, 1, 2).getTime(),
 ];
