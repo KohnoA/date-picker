6ec6d4dcada1372f56717a884e8198c1ba3793ce
KohnoA <san.kohno@yandex.ru>
2024-02-21 01:24:21 +0300

    feat: created general tests for DatePicker component

--- src/components/DatePicker/index.test.tsx
+++ src/components/DatePicker/index.test.tsx
@@ -1,6 +1,12 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 
-import { CalendarView, CalendarWeekStart } from '@/constants';
+import {
+  CalendarView,
+  CalendarWeekStart,
+  ERROR_VALUE_LESS_MIN,
+  ERROR_VALUE_MORE_MAX,
+  MONTH_NAMES,
+} from '@/constants';
 import { theme } from '@/styles';
 
 import '@testing-library/jest-dom';
@@ -119,5 +125,152 @@ describe('Testing the DatePicker Component Configuration', () => {
     render(<DatePicker showWeekends weekStart={CalendarWeekStart.MONDAY} />);
 
     expect(screen.getAllByTestId('calendar-day-name')[0]).toHaveTextContent('Mo');
+  });
+});
+
+describe('General tests for DatePicker component', () => {
+  it('Clear button should be hidden when date input is empty', () => {
+    render(<DatePicker />);
+
+    const dateInput = screen.getByTestId('date-input-field');
+
+    expect(dateInput).toHaveValue('');
+    expect(screen.queryByTestId('clear-date-input-button')).not.toBeInTheDocument();
+  });
+
+  it('Clear button should be displayed, when date input is not empty', () => {
+    render(<DatePicker />);
+
+    const dateInput = screen.getByTestId('date-input-field');
+    const enteredValue = '12/12/2024';
+
+    fireEvent.change(dateInput, { target: { value: enteredValue } });
+    expect(dateInput).toHaveValue(enteredValue);
+    expect(screen.getByTestId('clear-date-input-button')).toBeInTheDocument();
+  });
+
+  it('The input should be cleared when the clear button is pressed', () => {
+    render(<DatePicker />);
+
+    const dateInput = screen.getByTestId('date-input-field');
+    const enteredValue = '12/12/2024';
+
+    fireEvent.change(dateInput, { target: { value: enteredValue } });
+    expect(dateInput).toHaveValue(enteredValue);
+
+    const clearDateInputButton = screen.getByTestId('clear-date-input-button');
+    fireEvent.click(clearDateInputButton);
+    expect(dateInput).toHaveValue('');
+  });
+
+  it('An error message should be displayed if the value is greater than the maximum date', () => {
+    render(<DatePicker max={new Date(2024, 1, 2)} />);
+
+    const dateMoreThanMax = '01/02/2025';
+
+    const dateInput = screen.getByTestId('date-input-field');
+    fireEvent.change(dateInput, { target: { value: dateMoreThanMax } });
+
+    const errorMessage = screen.getByTestId('date-input-error');
+    expect(errorMessage).toBeInTheDocument();
+    expect(errorMessage).toHaveTextContent(ERROR_VALUE_MORE_MAX);
+  });
+
+  it('An error message should be displayed if the value is less than the minimum date', () => {
+    render(<DatePicker min={new Date(2024, 1, 2)} />);
+
+    const dateMoreThanMax = '01/02/2023';
+
+    const dateInput = screen.getByTestId('date-input-field');
+    fireEvent.change(dateInput, { target: { value: dateMoreThanMax } });
+
+    const errorMessage = screen.getByTestId('date-input-error');
+    expect(errorMessage).toBeInTheDocument();
+    expect(errorMessage).toHaveTextContent(ERROR_VALUE_LESS_MIN);
+  });
+
+  it('The calendar should open and close when clicked toggle calendar button', () => {
+    render(<DatePicker />);
+
+    const toggleCalendarButton = screen.getByTestId('toggle-calendar-button');
+    const calendar = screen.getByTestId('calendar');
+
+    expect(toggleCalendarButton).toBeInTheDocument();
+    expect(calendar).toHaveStyle('visibility: hidden;');
+
+    fireEvent.click(toggleCalendarButton);
+    expect(calendar).toHaveStyle('visibility: visible;');
+
+    fireEvent.click(toggleCalendarButton);
+    expect(calendar).toHaveStyle('visibility: hidden;');
+  });
+
+  it('If the value is not selected, the clear calendar button should be hidden', () => {
+    render(<DatePicker />);
+
+    expect(screen.queryByTestId('calendar-clear-button')).not.toBeInTheDocument();
+  });
+
+  it('If selected, the clear calendar button should be visible', () => {
+    render(<DatePicker initialDate={new Date(2024, 0, 1)} />);
+
+    fireEvent.click(screen.getByTestId('toggle-calendar-button'));
+    expect(screen.getByTestId('calendar-clear-button')).toBeInTheDocument();
+  });
+
+  it('Should move correctly by weeks', () => {
+    const activeDay = new Date(2024, 0, 1);
+
+    render(<DatePicker initialDate={activeDay} view={CalendarView.WEEK} />);
+
+    expect(screen.getByTestId('week-month-year')).toHaveTextContent(
+      `1 week of ${MONTH_NAMES[0]} 2024`,
+    );
+
+    const [prevWeek, nextWeek] = screen.getAllByTestId('rewind-week-month-button');
+
+    fireEvent.click(prevWeek);
+    expect(screen.getByTestId('week-month-year')).toHaveTextContent(
+      `6 week of ${MONTH_NAMES[MONTH_NAMES.length - 1]} 2023`,
+    );
+
+    fireEvent.click(nextWeek);
+    expect(screen.getByTestId('week-month-year')).toHaveTextContent(
+      `1 week of ${MONTH_NAMES[0]} 2024`,
+    );
+  });
+
+  it('Should move correctly by months', () => {
+    const activeDay = new Date(2024, 0, 1);
+
+    render(<DatePicker initialDate={activeDay} view={CalendarView.MONTH} />);
+
+    expect(screen.getByTestId('week-month-year')).toHaveTextContent(`${MONTH_NAMES[0]} 2024`);
+
+    const [prevMonth, nextMonth] = screen.getAllByTestId('rewind-week-month-button');
+
+    fireEvent.click(prevMonth);
+    expect(screen.getByTestId('week-month-year')).toHaveTextContent(
+      `${MONTH_NAMES[MONTH_NAMES.length - 1]} 2023`,
+    );
+
+    fireEvent.click(nextMonth);
+    expect(screen.getByTestId('week-month-year')).toHaveTextContent(`${MONTH_NAMES[0]} 2024`);
+  });
+
+  it('Should move correctly by years', () => {
+    const activeDay = new Date(2024, 0, 1);
+
+    render(<DatePicker initialDate={activeDay} view={CalendarView.YEAR} />);
+
+    expect(screen.getByTestId('week-month-year')).toHaveTextContent(`${MONTH_NAMES[0]} 2024`);
+
+    const [prevYear, nextYear] = screen.getAllByTestId('rewind-year-button');
+
+    fireEvent.click(prevYear);
+    expect(screen.getByTestId('week-month-year')).toHaveTextContent(`${MONTH_NAMES[0]} 2023`);
+
+    fireEvent.click(nextYear);
+    expect(screen.getByTestId('week-month-year')).toHaveTextContent(`${MONTH_NAMES[0]} 2024`);
   });
 });
