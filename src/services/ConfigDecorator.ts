fa237717e3da2d5884ed8923b8d063c18cfb5d70
KohnoA <san.kohno@yandex.ru>
2024-02-22 01:18:23 +0300

    feat: added default values for config props

--- src/services/ConfigDecorator.ts
+++ src/services/ConfigDecorator.ts
@@ -2,17 +2,25 @@ import { ComponentType } from 'react';
 
 import { RangeDatePicker } from '@/components/RangeDatePicker';
 import { SimpleDatePicker } from '@/components/SimpleDatePicker';
+import { CalendarView } from '@/constants';
 import { withProviders, withRangeLogic, withSimpleLogic } from '@/hocs';
 import { DatePickerConfigType } from '@/types';
+
+const defaultConfig: Pick<DatePickerConfigType, 'showWeekends' | 'view'> = {
+  showWeekends: true,
+  view: CalendarView.MONTH,
+};
 
 export class ConfigDecorator {
   public DatePickerComponent: ComponentType;
 
   constructor(config: DatePickerConfigType) {
+    const resultConfig = { ...defaultConfig, ...config };
+
     if (config.range) {
-      this.createRangeDatePicker(config);
+      this.createRangeDatePicker(resultConfig);
     } else {
-      this.createSimpleDatePicker(config);
+      this.createSimpleDatePicker(resultConfig);
     }
   }
 
