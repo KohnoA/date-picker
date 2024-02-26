761632805491b44f25daaa06fbb74377ab852496
KohnoA <san.kohno@yandex.ru>
2024-02-26 17:36:21 +0300

    refactor: WeekDaysName component

--- src/components/Calendar/WeekDaysName/styled.ts
+++ src/components/Calendar/WeekDaysName/styled.ts
@@ -2,7 +2,9 @@ import styled from 'styled-components';
 
 import { calendarGrid, flex } from '@/styles';
 
-export const DaysNameList = styled.ul<{ $showWeekends: boolean }>`
+import { DaysNameListProps } from './types';
+
+export const DaysNameList = styled.ul<DaysNameListProps>`
   ${({ $showWeekends }) => ($showWeekends ? calendarGrid(7) : calendarGrid(5, '44px'))}
 
   padding: 0;
